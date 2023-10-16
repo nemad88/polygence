5541d7807da36c29c1a87383c2d2a8fd8f39198b
nemadam88 <nemethadam88@gmail.com>
2023-10-16 15:14:52 +0200

    create main layout with basic style

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,194 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home(props) {
+  const { spendings } = props;
+
+  const currencies = ["ALL", "HUF", "USD"];
+
+  const [spending, setSpending] = useState();
+  const [amount, setAmount] = useState();
+  const [newSpendCurrency, setNewSpendCurrency] = useState(currencies[1]);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [filteredCurrency, setFilteredCurrency] = useState("ALL");
+
+  const icons = {
+    usd: "$",
+    huf: "Ft",
+  };
+
+  const inputClassNames = "rounded-lg shadow-lg px-2 py-1";
+  const filterButtonInactive = `${inputClassNames} flex-grow bg-white text-left`;
+  const filterButtonActive = `${inputClassNames} flex-grow bg-sky-200 font-bold text-left text-sky-700`;
+  const listStyle = `flex flex-col mt-6 w-[800px]`;
+  const listRowStyle = `flex flex-row justify-between rounded-lg shadow-lg mb-4 px-4 py-4 bg-white w-full`;
+
+  const handleNewSpendCurrencyChange = (currency) => {
+    setNewSpendCurrency(currency);
+    setDropdownVisible(false);
+  };
+
+  const checkIsSpendingValid = (spending) => {
+    for (const key in spending) {
+      if (
+        spending[key] === false ||
+        spending[key] === 0 ||
+        spending[key] === null ||
+        spending[key] === undefined
+      ) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const getFormattedDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { year: "numeric", month: "long", day: "2-digit" };
+    const timeOptions = { hour: "2-digit", minute: "2-digit", hour12: true };
+
+    const datePart = date.toLocaleDateString("en-US", options);
+    const timePart = date.toLocaleTimeString("en-US", timeOptions);
+
+    return `${timePart} - ${datePart}`;
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-start p-24 ${inter.className} `}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <form className="w-[800px] z-20">
+        <div className="flex w-full  space-x-4">
+          <input
+            type="search"
+            placeholder="description"
+            className={`${inputClassNames} w-3/6`}
+          ></input>
+          <input
+            className={`${inputClassNames} w-1/6 `}
+            placeholder="0"
+          ></input>
+          <div className="relative flex-grow flex">
+            <button
+              onClick={() => setDropdownVisible(!dropdownVisible)}
+              id="dropdown-button"
+              data-dropdown-toggle="dropdown"
+              type="button"
+              className={`${inputClassNames} flex-grow bg-white font-bold text-left`}
+            >
+              {newSpendCurrency}
+            </button>
+
+            {dropdownVisible && (
+              <ul className="absolute left-0 mt-9 bg-white rounded-lg p-2">
+                {currencies.slice(1).map((currency) => {
+                  return (
+                    <li
+                      className="cursor-pointer hover:bg-emerald-500 hover:text-white hover:rounded-lg hover:shadow-lg px-2 py-1 "
+                      key={currency}
+                      onClick={() => handleNewSpendCurrencyChange(currency)}
+                    >
+                      {currency}
+                    </li>
+                  );
+                })}
+
+                {/* <li onClick={() => handleNewSpendCurrencyChange("HUF")}>HUF</li> */}
+              </ul>
+            )}
+          </div>
+          <button
+            className={`${inputClassNames} flex-grow bg-emerald-500 font-bold text-white`}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Save
+          </button>
+        </div>
+      </form>
+
+      <div className="flex justify-between mt-16 w-[800px]">
+        <div>
+          <div className="relative flex">
+            <button
+              id="dropdown-button"
+              data-dropdown-toggle="dropdown"
+              type="button"
+              className={`${inputClassNames} flex-grow bg-white text-left`}
+            >
+              Sort by Date descending (default)
+            </button>
+            {/* <ul className="absolute left-0 mt-10 bg-white rounded-lg p-2">
+                <li>Sort by Date ascending</li>
+                <li>Sort by Amount</li>
+              </ul> */}
+          </div>
+        </div>
+        <div className="relative flex space-x-4">
+          {currencies.map((currency) => {
+            const style =
+              currency === filteredCurrency
+                ? filterButtonActive
+                : filterButtonInactive;
+
+            return (
+              <button
+                onClick={() => setFilteredCurrency(currency)}
+                className={style}
+                key={currency}
+              >
+                {currency}
+              </button>
+            );
+          })}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <ul className={`${listStyle} `}>
+        {spendings
+          // .slice(0, 20)
+          .filter(checkIsSpendingValid)
+          .map((spending) => {
+            const date = new Date(spending.spent_at);
+            return (
+              <li className={listRowStyle} key={spending.id}>
+                <div className="flex flex-row justify-between">
+                  <div
+                    className={`${filterButtonActive} mr-4 flex justify-center items-center w-10 h-10 text-xl`}
+                  >
+                    {icons[spending.currency.toLowerCase()]}
+                  </div>
+                  <div>
+                    <h3>{spending.description}</h3>
+                    <h4>{getFormattedDate(spending.spent_at)}</h4>
+                  </div>
+                </div>
+                <div className="flex space-x-4">
+                  <div className="flex justify-center items-center w-10 h-10 text-xl">
+                    $12.00
+                  </div>
+                  <div className="flex space-x-4 text-xl justify-center">
+                    <div className="flex justify-center items-center w-10 h-10 text-xl">
+                      ✎
+                    </div>
+                    <div className="flex justify-center items-center w-10 h-10 text-xl">
+                      x
+                    </div>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
     </main>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const URL =
+    "https://shielded-depths-43687-bb049deacd16.herokuapp.com/spendings/";
+  const res = await fetch(URL);
+  const spendings = await res.json();
+  return { props: { spendings } };
 }
